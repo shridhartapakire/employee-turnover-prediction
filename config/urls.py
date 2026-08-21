52762fca6ac32af4e448b11e3082b4790263b756
Shridhar <shridhartapakire4051@gmail.com>
2026-08-21 22:29:51 +0530

    Create initial home page template

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from .views import home
-
+from core.views import home
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home, name="home"),
     path("", home),
 ]
