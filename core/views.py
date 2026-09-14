b7b75e6b7149f7e557e590a1955dc849899d9fb2
Shridhar <shridhartapakire4051@gmail.com>
2026-09-14 21:21:33 +0530

    complete end to end application testing

--- core/views.py
+++ core/views.py
@@ -62,7 +62,7 @@ def upload_dataset(request):
         )
 
         average_probability = round(
-            results["Turnover_Probability"].mean() * 100,
+            results["Turnover_Probability"].mean(),
             2,
         )
 
